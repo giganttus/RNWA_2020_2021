3e2f0ba05fa9b77f9a326bee4c6cb9eef57423dc
giganttus <domagojpr@gmail.com>
2021-04-12 18:19:00 +0200

    DZ002_001

---
+++ DZ 2 - 01 AJAX JS/search.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  border-collapse: collapse;
+  border:solid 2px gray;
+  width: 100%;
+}
+
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {background-color: #f2f2f2;}
+</style>
+</head>
+<body>
+
+<?php
+$s = $_REQUEST["s"];
+$hint = "";
+
+// Konekcija baze
+$connection = mysqli_connect('localhost','root','','birt');
+
+if (!$connection) {
+    error_log("Failed to connect to MySQL: " . mysqli_error($connection));
+    die('Internal server error');
+}
+
+// Odabir baze
+$db_select = mysqli_select_db($connection, 'birt');
+if (!$db_select) {
+    error_log("Database selection failed: " . mysqli_error($connection));
+    die('Internal server error');
+}else
+{
+$sql="SELECT * FROM `employees` WHERE `firstName` LIKE  '%$s%' OR `lastName` LIKE  '%$s%'";
+
+$response = $connection->query($sql)or die("Querry failed");
+
+echo "<table>
+<tr>
+<th>Name</th>
+<th>Last name</th>
+</tr>";
+while ($myrow=mysqli_fetch_row($response)){
+			echo "<tr>";
+			  echo "<td>" . $myrow[2] . "</td>";
+			  echo "<td>" . $myrow[1] . "</td>";
+			  echo "</tr>";
+		}
+
+echo "</table>";
+}
+?>
+</body>
+</html>
