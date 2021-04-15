ac035100f56fd63e65f22ea4a34c529b8c1fd373
giganttus <domagojpr@gmail.com>
2021-04-15 20:50:51 +0200

    DZ003_001

---
+++ DZ 3 - 01 WSDL SOAP/soap/client.php
@@ -0,0 +1,26 @@
+<?php
+
+header('Content-Type: text/plain');
+
+try{
+	ini_set('soap.wsdl_cache_enabled',0);
+	ini_set('soap.wsdl_cache_ttl',0);
+
+	$value = $_POST['value'];
+	if($_POST['currency'] === "toEur"){
+		$sClient = new SoapClient('toEur.wsdl');
+		$response = $sClient->cBTE($value);
+	}
+	else{
+		$sClient = new SoapClient('toBam.wsdl');
+		$response = $sClient->cETB($value);
+	}
+	
+	var_dump($response);
+
+} catch(SoapFault $e){
+  var_dump($e);
+  echo $e;
+}
+
+?>
